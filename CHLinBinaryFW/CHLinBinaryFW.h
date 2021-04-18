51b80f3f5afc6e7a5ce04dd5595914c954e9b7ca
priyankavachhani@callhippo.com <priyankavachhani@callhippo.com>
2021-04-18 18:01:22 +0530

    Initial project setup

---
+++ CHLinBinaryFW/CHLinBinaryFW.h
@@ -0,0 +1,19 @@
+//
+//  CHLinBinaryFW.h
+//  CHLinBinaryFW
+//
+//  Created by Apple on 18/04/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHLinBinaryFW.
+FOUNDATION_EXPORT double CHLinBinaryFWVersionNumber;
+
+//! Project version string for CHLinBinaryFW.
+FOUNDATION_EXPORT const unsigned char CHLinBinaryFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHLinBinaryFW/PublicHeader.h>
+
+
